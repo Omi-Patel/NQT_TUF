afbb5cd5c7e608062cf54ce85bc205acb4298681
Om Patel <omupatel7113@gmail.com>
2024-08-14 19:35:39 +0530

    Problem 1

---
+++ FindMin.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class FindMin{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the size of the Array : ");
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		
+		System.out.print("Enter Array Element : ");
+		
+		for(int i=0; i<n; i++){
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println("Smallest Element : " + arr[0]);
+	}
+}
