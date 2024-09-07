a8ea9556f5c03a1b073ef1ce1162afd620c829a1
Om Patel <omupatel7113@gmail.com>
2024-09-07 10:21:29 +0530

    Update condition

--- LeapYearOrNot.java
+++ LeapYearOrNot.java
@@ -6,7 +6,7 @@ class LeapYearOrNot{
 		System.out.print("Enter any Year : ");
 		int y = sc.nextInt();
 		
-		if(y % 4 == 0){
+		if(((year % 4 == 0) && (year % 100 != 0)) ||(year % 400 == 0)){
 			System.out.println(y + " is a Leap Year.");
 		} else {
 			System.out.println(y + " is not a Leap Year.");
